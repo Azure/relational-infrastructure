4cf3b7abf8b03dd80f2e41cb7b2b028db515fb11
Casey Watson <cawatson@microsoft.com>
2025-01-23 13:56:27 -0600

    Completed networks

--- epic/main.tf
+++ epic/main.tf
@@ -113,5 +113,106 @@ module "epic" {
         "primary_shared_infra"
       ]
     }
+
+    alt_dmz = {
+      location_name = "alt"
+      name          = "${var.deployment_prefix}-${var.locations["alt"]}-dmz-net01"
+      address_space = var.networks.alt.dmz.address_space
+
+      subnets = {
+        firewall = {
+          name          = "FirewallSubnet"
+          address_space = var.networks.alt.dmz.subnets.firewall.address_space
+        }
+        production = {
+          address_space = var.networks.alt.dmz.subnets.production.address_space
+        }
+        non_production = {
+          address_space = var.networks.alt.dmz.subnets.non_production.address_space
+        }
+      }
+    }
+
+    alt_shared_infra = {
+      location_name = "alt"
+      name          = "${var.deployment_prefix}-${var.locations["alt"]}-shared-infra-net01"
+      address_space = var.networks.alt.shared_infra.address_space
+
+      subnets = {
+        gateway = {
+          name          = "GatewaySubnet"
+          address_space = var.networks.alt.shared_infra.subnets.gateway.address_space
+        }
+        management = {
+          address_space = var.networks.alt.shared_infra.subnets.management.address_space
+        }
+      }
+
+      peered_to = [
+        "alt_hyperspace",
+        "alt_hyperspace_web",
+        "alt_main"
+      ]
+    }
+
+    alt_main = {
+      location_name = "alt"
+      name          = "${var.deployment_prefix}-${var.locations["alt"]}-main-net01"
+      address_space = var.networks.alt.main.address_space
+
+      subnets = {
+        cogito = {
+          address_space = var.networks.alt.main.subnets.cogito.address_space
+        }
+        odb = {
+          address_space = var.networks.alt.main.subnets.odb.address_space
+        }
+        wss = {
+          address_space = var.networks.alt.main.subnets.wss.address_space
+        }
+      }
+
+      peered_to = [
+        "alt_hyperspace",
+        "alt_hyperspace_web",
+        "alt_shared_infra"
+      ]
+    }
+
+    alt_hyperspace = {
+      location_name = "alt"
+      name          = "${var.deployment_prefix}-${var.locations["alt"]}-hyperspace-net01"
+      address_space = var.networks.alt.hyperspace.address_space
+
+      subnets = {
+        hyperspace = {
+          address_space = var.networks.alt.hyperspace.subnets.hyperspace.address_space
+        }
+      }
+
+      peered_to = [
+        "alt_hyperspace_web",
+        "alt_main",
+        "alt_shared_infra"
+      ]
+    }
+
+    alt_hyperspace_web = {
+      location_name = "alt"
+      name          = "${var.deployment_prefix}-${var.locations["alt"]}-hyperspace-web-net01"
+      address_space = var.networks.alt.hyperspace_web.address_space
+
+      subnets = {
+        hyperspace_web = {
+          address_space = var.networks.alt.hyperspace_web.subnets.hyperspace_web.address_space
+        }
+      }
+
+      peered_to = [
+        "alt_hyperspace",
+        "alt_main",
+        "alt_shared_infra"
+      ]
+    }
   }
 }
